3b7ff843399885ed16484b9ccec547569ac03a65
amanda-w <amandanwong@gmail.com>
2022-12-17 23:13:13 -0800

    add: Veggie component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,10 +11,19 @@ function Popular() {
     }, []);
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-        const data = await api.json();
-        setPopular(data.recipes);
-        console.log(data.recipes);
+
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+            const data = await api.json();
+
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+            console.log(data.recipes);
+        }
     };
 
     return (
@@ -26,7 +35,7 @@ function Popular() {
                         perPage: 4,
                         pagination: false,
                         drag: 'free',
-                        gap: "5rem",
+                        gap: "3rem",
                     }}
                 >
                     {popular.map((recipe) => {
